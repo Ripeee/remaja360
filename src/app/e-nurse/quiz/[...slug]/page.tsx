19c0683bef8f6a0c9bf956d61622d8e53c566214
ripeee <arythegodhand@gmail.com>
2024-12-31 20:36:43 +0700

    fix quiz

--- src/app/e-nurse/quiz/[...slug]/page.tsx
+++ src/app/e-nurse/quiz/[...slug]/page.tsx
@@ -5,14 +5,21 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import axios from "axios";
 import Image from "next/image";
+import {useRouter} from "next/navigation";
 
 export default function CategoryQuiz() {
+
+	const router = useRouter()
 
 	const searchParams = useSearchParams();
 
 	const id = searchParams.get('id');
 
-	const [selectedAnswer, setSelectedAnswer] = React.useState<number>(0);
+	const [isLoading, setIsLoading] = React.useState<boolean>(true);
+	const [selectedAnswers, setSelectedAnswers] = React.useState<number[]>([]);
+	const [correctAnswersCount, setCorrectAnswersCount] = React.useState<number>(0);
+	const [selectedAnswer, setSelectedAnswer] = React.useState<number | null>(null);
+  const [isQuizFinished, setIsQuizFinished] = React.useState<boolean>(false);
 	const [currentQuestionIndex, setCurrentQuestionIndex] =
 		React.useState<number>(0);
 	const [data, setData] = React.useState<{
@@ -35,24 +42,53 @@ export default function CategoryQuiz() {
 	};
 
 	function handleSelectedAnswer(id: number, qid: number) {
-		console.log(id, getCorrectOptionById(id, qid), 'lololol');
+		console.log(id, getCorrectOptionById(id, qid), "lololol");
+
+		const newSelectedAnswers = [...selectedAnswers];
+		const previousAnswer = newSelectedAnswers[currentQuestionIndex];
+		const isPreviouslyCorrect = previousAnswer ? getCorrectOptionById(previousAnswer, qid) : false;
+		const isNewCorrect = getCorrectOptionById(id, qid);
+
+		// Update the selected answer
+		newSelectedAnswers[currentQuestionIndex] = id;
+		setSelectedAnswers(newSelectedAnswers);
 		setSelectedAnswer(id);
+
+		// Adjust correctAnswersCount based on the answer correctness
+		if (!isPreviouslyCorrect && isNewCorrect) {
+			setCorrectAnswersCount((prev) => prev + 1);
+		} else if (isPreviouslyCorrect && !isNewCorrect) {
+			setCorrectAnswersCount((prev) => prev - 1);
+		}
 	}
 
 	const handleNextQuestion = () => {
 		if (currentQuestionIndex < (data.questions?.length ?? 0) - 1) {
 			setCurrentQuestionIndex(currentQuestionIndex + 1);
+			const nextAnswer = selectedAnswers[currentQuestionIndex + 1] || null;
+			setSelectedAnswer(nextAnswer); // Set selected answer for the next question
 		}
 	};
-	function result() {
-		console.log('Lihat Hasil')
-	}
 
 	const handlePrevQuestion = () => {
 		if (currentQuestionIndex > 0) {
 			setCurrentQuestionIndex(currentQuestionIndex - 1);
+			const prevAnswer = selectedAnswers[currentQuestionIndex - 1] || null;
+			setSelectedAnswer(prevAnswer); // Set selected answer for the previous question
 		}
 	};
+
+	const calculateScore = () => {
+		const totalQuestions = data.questions?.length ?? 0;
+		
+		return ((correctAnswersCount / totalQuestions) * 100).toFixed(2); // Score in percentage
+	};
+
+	function result() {
+		console.log(calculateScore(), id)
+		setIsQuizFinished(true);
+		// console.log(`Your score is: ${score} out of ${data.questions?.length}`);
+	}
 
 	const name = "KUCING";
 	
@@ -61,6 +97,7 @@ export default function CategoryQuiz() {
 			try {
 				const response = await axios.get(`/api/quiz?id=${id}`); // Replace with your API URL
 				setData(response.data); // Update state with fetched data
+				setIsLoading(false)
 			} catch (err: unknown) {
 				if (err instanceof Error) {
 					console.log(err.message);
@@ -88,73 +125,188 @@ export default function CategoryQuiz() {
 				Kembali Quiz
 			</Link>
 
-			<h1 className="font-bold text-center text-2xl underline">
-				<p>{data.category?.title}</p>
-			</h1>
-			<div className="flex justify-center">
-				<Image
-					src="/images/question.svg"
-					alt="Quiz"
-					width={400}
-					height={400}
-					className="mt-[-40px] mx-4 mr-[-40px] bg-fixed w-full h-5/6"
-				/>
-			</div>
-			{(data.questions?.length ?? 0) > 0 && (
-				<div>
-					<p className="text-xl mx-36 mt-[-300px] w-2/6 z-10">
-						{data.questions?.[currentQuestionIndex]?.question}
-					</p>
-					<div className="flex flex-col gap-4 mx-10 my-8 rounded-xl mt-36">
-						{data.questions?.[currentQuestionIndex]?.options?.map((o) => (
-							<button
-								key={o.id}
-								onClick={() =>
-									handleSelectedAnswer(
-										o.id,
-										data.questions?.[currentQuestionIndex]?.id ?? 0,
-									)
-								}
-								className={`bg-slate-400 rounded-xl w-full hover:bg-slate-700 ${
-									selectedAnswer === o.id && "bg-slate-700"
-								}`}>
-								<p className="text-white text-center font-bold text-md py-2">
-									{o.option}
-								</p>
-							</button>
-						))}
-					</div>
-				</div>
-			)}
+			{isLoading ? (
+				<>
+					<p className="text-center">Loading...</p>
+				</>
+			) : (
+				<>
+					{!isQuizFinished ? (
+						<>
+							<h1 className="font-bold text-center text-2xl underline">
+								<p>{data.category?.title}</p>
+							</h1>
+							<div className="flex justify-center">
+								<Image
+									src="/images/question.svg"
+									alt="Quiz"
+									width={400}
+									height={400}
+									className="mt-[-40px] mx-4 mr-[-40px] bg-fixed w-full h-5/6"
+								/>
+							</div>
+							{(data.questions?.length ?? 0) > 0 && (
+								<div>
+									<p className="text-xl mx-36 mt-[-300px] w-2/6 z-10">
+										{data.questions?.[currentQuestionIndex]?.question}
+									</p>
+									<div className="flex flex-col gap-4 mx-10 my-8 rounded-xl mt-36">
+										{data.questions?.[currentQuestionIndex]?.options?.map(
+											(o) => (
+												<button
+													key={o.id}
+													onClick={() =>
+														handleSelectedAnswer(
+															o.id,
+															data.questions?.[currentQuestionIndex]?.id ?? 0,
+														)
+													}
+													className={`bg-slate-400 rounded-xl w-full hover:bg-slate-700 ${
+														selectedAnswer === o.id && "bg-slate-700"
+													}`}>
+													<p className="text-white text-center font-bold text-md py-2">
+														{o.option}
+													</p>
+												</button>
+											),
+										)}
+									</div>
+								</div>
+							)}
 
-			<div className="flex justify-around mx-10 gap-8">
-				<button
-					onClick={handlePrevQuestion}
-					disabled={currentQuestionIndex === 0}
-					className="bg-blue-400 rounded-xl w-full hover:bg-sky-600 disabled:bg-blue-50">
-					<p className="text-white text-center font-bold text-md py-2">
-						Sebelumnya
-					</p>
-				</button>
-				<button
-					onClick={handleNextQuestion}
-					disabled={currentQuestionIndex >= (data?.questions?.length ?? 0) - 1}
-					className="bg-blue-400 rounded-xl w-full hover:bg-sky-600 disabled:bg-blue-50">
-					<p className="text-white text-center font-bold text-md py-2">
-						Selanjutnya
-					</p>
-				</button>
-			</div>
-			{currentQuestionIndex >= (data?.questions?.length ?? 0) - 1 && (
-				<div className="flex justify-center mx-auto">
-					<button
-						onClick={result}
-						className="bg-green-400 rounded-xl px-8 hover:bg-green-600 disabled:bg-blue-50">
-						<p className="text-white text-center font-bold text-md py-2">
-							Lihat Hasil
-						</p>
-					</button>
-				</div>
+							<div className="flex justify-around mx-10 gap-8">
+								<button
+									onClick={handlePrevQuestion}
+									disabled={currentQuestionIndex === 0}
+									className="bg-blue-400 rounded-xl w-full hover:bg-sky-600 disabled:bg-blue-50">
+									<p className="text-white text-center font-bold text-md py-2">
+										Sebelumnya
+									</p>
+								</button>
+								<button
+									onClick={handleNextQuestion}
+									disabled={
+										currentQuestionIndex >= (data?.questions?.length ?? 0) - 1
+									}
+									className="bg-blue-400 rounded-xl w-full hover:bg-sky-600 disabled:bg-blue-50">
+									<p className="text-white text-center font-bold text-md py-2">
+										Selanjutnya
+									</p>
+								</button>
+							</div>
+							{currentQuestionIndex >= (data?.questions?.length ?? 0) - 1 && (
+								<div className="flex justify-center mx-auto">
+									<button
+										onClick={result}
+										className="bg-green-400 rounded-xl px-8 hover:bg-green-600 disabled:bg-blue-50">
+										<p className="text-white text-center font-bold text-md py-2">
+											Lihat Hasil
+										</p>
+									</button>
+								</div>
+							)}
+						</>
+					) : (
+						<>
+							<h1 className="font-bold text-center text-4xl underline my-4">
+								{"HASIL QUIZ " + data.category?.title.toUpperCase()}
+							</h1>
+							{Number(calculateScore()) == 100 && (
+								<div className="mx-10 py-8 gap-4 flex flex-col bg-sky-200 rounded-2xl">
+									<Image
+										src={`/images/good-score.svg`}
+										alt="Quiz"
+										width={300}
+										height={300}
+										className="w-full h-60"
+									/>
+									<h1 className="font-bold text-center text-4xl">
+										{calculateScore()}%
+										<br />
+										SANGAT BAIK
+									</h1>
+									<p className="text-center text-xl mx-10">
+										Bagus! Kamu punya pemahaman yang solid tentang{" "}
+										{data.category?.title}. Terus jaga kesehatanmu ya!
+									</p>
+								</div>
+							)}
+							{Number(calculateScore()) >= 80 &&
+								Number(calculateScore()) < 100 && (
+									<div className="mx-10 py-8 gap-4 flex flex-col bg-sky-200 rounded-2xl">
+										<Image
+											src={`/images/nice_score.svg`}
+											alt="Quiz"
+											width={300}
+											height={300}
+											className="w-full h-60"
+										/>
+										<h1 className="font-bold text-center text-4xl">
+											{calculateScore()}%
+											<br />
+											BAIK
+										</h1>
+										<p className="text-center text-xl mx-10">
+											Bagus! Kamu punya pemahaman yang solid tentang{" "}
+											{data.category?.title}. Terus jaga kesehatanmu ya!
+										</p>
+									</div>
+								)}
+							{Number(calculateScore()) > 50 &&
+								Number(calculateScore()) < 80 && (
+									<div className="mx-10 py-8 gap-4 flex flex-col bg-sky-200 rounded-2xl">
+										<Image
+											src={`/images/bad_score.svg`}
+											alt="Quiz"
+											width={300}
+											height={300}
+											className="w-full h-60"
+										/>
+										<h1 className="font-bold text-center text-4xl">
+											{calculateScore()}%
+											<br />
+											KURANG
+										</h1>
+										<p className="text-center text-xl mx-10">
+											Hmm, sepertinya kamu perlu belajar lebih banyak tentang{" "}
+											{data.category?.title}. Ini kesempatan untuk mencari tahu
+											lebih banyak demi kesehatanmu!
+										</p>
+									</div>
+								)}
+							{Number(calculateScore()) <= 50 && (
+								<div className="mx-10 py-8 gap-4 flex flex-col bg-sky-200 rounded-2xl">
+									<Image
+										src={`/images/bad_score.svg`}
+										alt="Quiz"
+										width={300}
+										height={300}
+										className="w-full h-60"
+									/>
+									<h1 className="font-bold text-center text-4xl">
+										{calculateScore()}%
+										<br />
+										BURUK
+									</h1>
+									<p className="text-center text-xl mx-10">
+										Aduh, sepertinya kamu perlu lebih banyak info tentang{" "}
+										{data.category?.title}. Jangan ragu untuk belajar dan
+										diskusi, ya! Kesehatanmu penting!
+									</p>
+								</div>
+							)}
+							<div className="flex justify-center mx-auto my-8">
+								<button
+									onClick={() => router.back()}
+									className="bg-blue-400 rounded-xl px-8 hover:bg-blue-600 disabled:bg-blue-50">
+									<p className="text-white text-center font-bold text-md py-2">
+										Quiz Lainnya
+									</p>
+								</button>
+							</div>
+						</>
+					)}
+				</>
 			)}
 		</div>
 	);
